5f929a8c4c2f82968908b7ec32434b44393c23fe
vogtalex <vogtalex@oregonstate.edu>
2022-02-04 20:46:32 -0800

    Added CSV functionality and added simplification of gui code

---
+++ csv_gui.py
@@ -0,0 +1,58 @@
+import csv
+import os
+
+csvName = "response.csv"
+
+def writeToCSV(array):
+    with open(csvName, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+        writer.writerow(array)
+
+def readFromCSV(array):
+    with open(csvName, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', lineterminator='\n')
+        for row in reader:
+            array.append(row)
+
+def writeDataCSV(userPrediction, trueValue):
+    array = [userPrediction, trueValue]
+    writeToCSV(array)
+
+def outputCSV():
+    outputArray = []
+    readFromCSV(outputArray)
+    for idx in range(len(outputArray)):
+        print(outputArray[idx])
+
+def initializeCSV():
+    file = 'response.csv'
+    if(os.path.exists(file) and os.path.isfile(file)):
+        os.remove(file)
+
+def formatCSV():
+    csvName = 'formatted.csv'
+    outputArray = []
+    formattedArray = []
+    numberArray = []
+    totalNumString = ''
+    correctNumString = ''
+    correctCount = 0
+    totalCount = 0
+    readFromCSV(outputArray)
+
+    for idx in range(len(outputArray)):
+        if outputArray[idx][0] == outputArray[idx][1]:
+            correctCount += 1
+            correctNumString += str(outputArray[idx][1])
+        totalNumString += str(outputArray[idx][1])
+        totalCount += 1
+    
+    formattedArray.append(correctCount)
+    formattedArray.append(totalCount)
+    writeToCSV(formattedArray)
+
+    for x in range(10):
+        correct = correctNumString.count(str(x))
+        total = totalNumString.count(str(x))
+        numArray = [x, correct, total]
+        writeToCSV(numArray)
--- run_gui.py
+++ run_gui.py
@@ -1,4 +1,5 @@
 from models.net import *
+from csv_gui import *
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,12 +32,6 @@ from numpy import load
 
 images = load('data.npy', allow_pickle=True)
 
-# Initializes an image for GUI
-label, new_label, image, = images[0]
-plt.title("What is this number?")
-plt.imshow(image, cmap="gray")
-plt.savefig('saved_figure.png')
-
 
 # Generates an image of epsilon 0.15
 def generateNewImage(count):
@@ -60,7 +55,8 @@ path3 = "saved_figure3.png"
 
 # Count of image displayed currently and count guessed correctly in the GUI
 correctCount = 0
-totalCount = 1
+global totalCount 
+totalCount = 0
 
 # Iterates the total count to iterate through images
 
@@ -75,14 +71,40 @@ def quitFunction():
     root.destroy()
     quit()
 
+# Generates new model prediction
+def modelPrediction(totalCount):
+    stringModel = "Model Prediction: "
+    ga, answer, gar, = images[totalCount]
+    convAnswer = str(answer)
+    stringModel = stringModel + convAnswer
+    def_label = tk.Label(visual_aid_frame, text=stringModel)
+    def_label.pack(padx=10, pady=5, fill=tk.BOTH)
+
+#Opens image and places new image on gui
+def openImage():
+    image1 = Image.open(path2)
+    test = ImageTk.PhotoImage(image1, master=root)
+    label1 = tk.Label(image_frame, image=test)
+    label1.image = test
+    label1.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
+
 
 # Function for button for user guess
 def myClick():
-    totalCount = countIterator()
+    # Create new image
+    global totalCount
     global label
+
+    if totalCount == 0 :
+        label = generateNewImage(totalCount)
+        modelPrediction(totalCount)
+    
     currCount = correctCount
     newLabel = label
     currNum = e.get()
+
+    #Add guess to CSV
+    writeDataCSV(int(currNum), newLabel)
 
     if (int(currNum) == newLabel):
         currCount = currCount + 1
@@ -91,26 +113,18 @@ def myClick():
     else:
         myLabel2 = Label(output_frame, text="Incorrect")
         myLabel2.pack(padx=10, pady=5, fill=tk.BOTH)
-
-    # Create new image
+    
+    totalCount = countIterator()
     label = generateNewImage(totalCount)
+    openImage()
 
-    image1 = Image.open(path2)
-    test = ImageTk.PhotoImage(image1, master=root)
-    label1 = tk.Label(image_frame, image=test)
-    label1.image = test
-    label1.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
 
-    # Generates new model prediction
-    stringModel = "Model Prediction: "
-    ga, answer, gar, = images[totalCount]
-    convAnswer = str(answer)
-    stringModel = stringModel + convAnswer
-    def_label = tk.Label(visual_aid_frame, text=stringModel)
-    def_label.pack(padx=10, pady=5, fill=tk.BOTH)
 
-    #Label(image_frame, image=test).grid(row=1, column=0, sticky="nsew", padx=2, pady=2)
-    totalCount = totalCount + 1
+
+
+#Initialize CSV by deleting prior csv "response.csv"
+initializeCSV()
+
 
 
 # GUI
@@ -140,7 +154,8 @@ root.grid_columnconfigure(2, weight=2)
 
 
 # Create a photoimage object of the image in the path
-image1 = Image.open(path1)
+generateNewImage(0)
+image1 = Image.open(path2)
 test = ImageTk.PhotoImage(image1, master=root)
 Label(image_frame, image=test).grid(
     row=0, column=0, sticky="nsew", padx=2, pady=2)
@@ -168,3 +183,7 @@ exit_button.grid(row=3, column=0, pady=20)
 
 # Loop
 root.mainloop()
+
+#Output CSV
+formatCSV()
+outputCSV()
