fb33e780314189ee7d802a4352679770a3e8e693
Simon Millsap <simonmillsap@gmail.com>
2022-02-15 11:35:45 -0800

    npys too big now, updated heatmap coloring

--- run_gui.py
+++ run_gui.py
@@ -40,6 +40,12 @@ test_loader = torch.utils.data.DataLoader(
     ])),
     batch_size=1, shuffle=True)
 
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+    ])),
+    batch_size=1, shuffle=True)
+
 # Starter code if using cuda
 use_cuda = True
 device = torch.device("cuda:0" if (
@@ -72,7 +78,70 @@ def fgsm_attack(image, epsilon, data_grad):
     # Return the perturbed image
     return perturbed_image
 
-def gen_adv_features():
+def gen_adv_features_test():
+    model.eval()
+    cnt = 0
+
+    labels = []
+
+    out_data = []
+    out_adv_data= []
+
+    #out_target = []
+
+    #out_output = []
+    out_adv_output = []
+
+    for data, target in test_loader:
+        cnt += 1
+        print("processing: %d/%d" % (cnt, len(test_loader.dataset)))
+
+        delta = fgsm(model,data,target,0.2)
+        #delta = pgd_linf(model,data,target,0.1,1e-2,40)
+
+        #output = model(data)
+        adv_output = model(data + delta)
+
+        #output_np = output.data.cpu().numpy()
+        adv_output_np = adv_output.data.cpu().numpy()
+        #print(np.argmax(output_np),np.argmax(adv_output_np))
+
+
+        labels.append(target.numpy()[0])
+        #target_np = output.max(1, keepdim=True)[1][0].numpy()
+
+        adv_data = data+delta
+        ######################
+
+        #out_output.append(output_np)
+        out_adv_output.append(adv_output_np)
+
+        #out_target.append(target_np[:, np.newaxis])
+
+        #data = torch.flatten(data,2,3)
+        #data = torch.flatten(data,0)
+        #out_data.append(data.numpy())
+
+        adv_data = torch.flatten(adv_data,2,3)
+        adv_data = torch.flatten(adv_data,0)
+        out_adv_data.append(adv_data.numpy())
+
+
+    #output_array = np.concatenate(out_output, axis=0)
+    adv_output_array = np.concatenate(out_adv_output, axis=0)
+    #target_array = np.concatenate(out_target, axis=0)
+
+    #np.save('./npys/output.npy', output_array, allow_pickle=False)
+    np.save('./npys/testlabels.npy', labels, allow_pickle=False)
+    np.save('./npys/advoutput.npy', adv_output_array, allow_pickle=False)
+    #np.save('./npys/target.npy', target_array, allow_pickle=False)
+    #np.save('./npys/data.npy',out_data,allow_pickle=False)
+    np.save('./npys/advdata.npy',out_adv_data,allow_pickle=False)
+    #torch.save(out_data,'./npys/data.npy')
+    #torch.save(out_adv_data,'./npys/advdata.npy')
+
+
+def gen_adv_features_train():
     model.eval()
     cnt = 0
 
@@ -85,29 +154,29 @@ def gen_adv_features():
 
     out_output = []
     out_adv_output = []
-    for data, target in test_loader:
+    for data, target in train_loader:
         cnt += 1
-        print("processing: %d/%d" % (cnt, len(test_loader.dataset)))
+        print("processing: %d/%d" % (cnt, len(train_loader.dataset)))
 
         #delta = fgsm(model,data,target,0.2)
-        delta = pgd_linf(model,data,target,0.1,1e-2,40)
+        #delta = pgd_linf(model,data,target,0.1,1e-2,40)
 
         output = model(data)
-        adv_output = model(data + delta)
+        #adv_output = model(data + delta)
 
         output_np = output.data.cpu().numpy()
-        adv_output_np = adv_output.data.cpu().numpy()
+        #adv_output_np = adv_output.data.cpu().numpy()
         #print(np.argmax(output_np),np.argmax(adv_output_np))
 
 
         labels.append(target.numpy()[0])
         target_np = output.max(1, keepdim=True)[1][0].numpy()
 
-        adv_data = data+delta
+        #adv_data = data+delta
         ######################
 
         out_output.append(output_np)
-        out_adv_output.append(adv_output_np)
+        #out_adv_output.append(adv_output_np)
 
         out_target.append(target_np[:, np.newaxis])
 
@@ -115,26 +184,26 @@ def gen_adv_features():
         data = torch.flatten(data,0)
         out_data.append(data.numpy())
 
-        adv_data = torch.flatten(adv_data,2,3)
-        adv_data = torch.flatten(adv_data,0)
-        out_adv_data.append(adv_data.numpy())
+        #adv_data = torch.flatten(adv_data,2,3)
+        #adv_data = torch.flatten(adv_data,0)
+        #out_adv_data.append(adv_data.numpy())
 
 
     output_array = np.concatenate(out_output, axis=0)
-    adv_output_array = np.concatenate(out_adv_output, axis=0)
+    #adv_output_array = np.concatenate(out_adv_output, axis=0)
     target_array = np.concatenate(out_target, axis=0)
 
-    np.save('./npys/output.npy', output_array, allow_pickle=False)
-    np.save('./npys/labels.npy', labels, allow_pickle=False)
-    np.save('./npys/advoutput.npy', adv_output_array, allow_pickle=False)
-    np.save('./npys/target.npy', target_array, allow_pickle=False)
-    np.save('./npys/data.npy',out_data,allow_pickle=False)
-    np.save('./npys/advdata.npy',out_adv_data,allow_pickle=False)
+    np.save('./npys/trainoutput.npy', output_array, allow_pickle=False)
+    np.save('./npys/trainlabels.npy', labels, allow_pickle=False)
+    #np.save('./npys/advoutput.npy', adv_output_array, allow_pickle=False)
+    np.save('./npys/traintarget.npy', target_array, allow_pickle=False)
+    np.save('./npys/traindata.npy',out_data,allow_pickle=False)
+    #np.save('./npys/advdata.npy',out_adv_data,allow_pickle=False)
     #torch.save(out_data,'./npys/data.npy')
     #torch.save(out_adv_data,'./npys/advdata.npy')
 
-
-gen_adv_features()
+gen_adv_features_test()
+#gen_adv_features_train()
 exit(0)
 
 
--- tsne_run.py
+++ tsne_run.py
@@ -6,23 +6,17 @@ from sklearn import datasets
 from sklearn.manifold import TSNE
 from functions import *
 import torch
-limit = 10000
-idx = 0
+limit = 60000
+idx = 7
 
-labelling = np.load('./npys/labels.npy').astype(np.float64)[:limit]
-output = np.load('./npys/output.npy').astype(np.float64)[:limit]
-data = np.load('./npys/data.npy').astype(np.float64)[:limit]
-#data = torch.flatten(data,2,3)
-#data = torch.flatten(data,1)
-#data = data.numpy()[:limit]
-target = np.load('./npys/target.npy')[:limit]
+trainlabels = np.load('./npys/trainlabels.npy').astype(np.float64)[:limit]
+testlabels = np.load('./npys/testlabels.npy').astype(np.float64)[:limit]
+trainoutput = np.load('./npys/trainoutput.npy').astype(np.float64)[:limit]
+traindata = np.load('./npys/traindata.npy').astype(np.float64)[:limit]
+#traintarget = np.load('./npys/traintarget.npy')[:limit]
 
 advoutput = np.load('./npys/advoutput.npy').astype(np.float64)[:limit]
-
 advdata = np.load('./npys/advdata.npy').astype(np.float64)[:limit]
-#advdata = torch.flatten(advdata,2,3)
-#advdata = torch.flatten(advdata,1)
-#advdata = advdata.numpy()[:limit]
 
 
 def findNearest(data,advdata,idx,k=10):
@@ -31,36 +25,67 @@ def findNearest(data,advdata,idx,k=10):
     label = np.argmax(advoutput[idx])
     print("Model prediction: ", label)
 
-    l = data - example
+    l = traindata - example
 
     norms = np.linalg.norm(l,axis=1)
 
+
     top = np.argpartition(norms,k-1)
-    #print(idx,int(labelling[idx]))
-    print("True label: ", int(labelling[idx]))
+
+    ########
+    print("True label: ", int(testlabels[idx]))
     print("Nearest 10 labels: ")
-    print([(int(labelling[idx])) for idx in top[:k]])
-    print("Distance to nearest 10 points: ")
-    print([(norms[idx]) for idx in top[:k]])
-    return norms, top[:k]
+    print(top[:k])
+    print([(int(trainlabels[i])) for i in top[:k]])
+    #print("Distance to nearest 10 points: ")
+    print([(norms[idx]) for idx in top[1:k]])
+    return norms, top[1:k],label,int(testlabels[idx])
 
-norms,idxs = findNearest(data,advdata,idx)
+def findNearestTrue(data,advdata,idx,k=10):
+    print("Index: ",idx)
+    example = traindata[idx]
+    label = np.argmax(trainoutput[idx])
+    print("Model prediction: ", label)
 
-print('data shape: ', data.shape)
-print('target shape: ', target.shape)
-print('output shape: ', output.shape)
+    l = traindata - example
+
+    norms = np.linalg.norm(l,axis=1)
+
+
+    top = np.argpartition(norms,k-1)
+
+    ########
+    print("True label: ", int(trainlabels[idx]))
+    print("Nearest 10 labels: ")
+    print([(int(trainlabels[idx])) for idx in top[:k]])
+    #print("Distance to nearest 10 points: ")
+    print([(norms[idx]) for idx in top[1:k]])
+    return norms, top[1:k],label,int(trainlabels[idx])
+
+norms,idxs,prediction,truelabel = findNearestTrue(traindata,advdata,idx)
+#norms,idxs,prediction,truelabel = findNearest(traindata,advdata,idx)
+
+print('data shape: ', traindata.shape)
+print('labels shape: ', trainlabels.shape)
+print('output shape: ', trainoutput.shape)
 #print(data[0])
 
-fig, ax = plt.subplots()
+fig, (ax1,ax2) = plt.subplots(1,2)
 
-print(data.shape)
+print(traindata.shape)
 print(advdata.shape)
 #for combining data/advdata
-data = np.append(data, advdata, axis=0)
+#data = np.append(data, advdata, axis=0)
 
-print(data.shape)
-tsne = TSNE(n_components=2, random_state=3,perplexity=100)
-X_2d = tsne.fit_transform(data)
+print(traindata.shape)
+
+X_2d = []
+if exists("./npys/embedding.npy"):
+    X_2d = np.load('./npys/embedding.npy').astype(np.float64)
+else:
+    tsne = TSNE(n_components=2, random_state=3,perplexity=100)
+    X_2d = tsne.fit_transform(traindata)
+    np.save('./npys/embedding.npy', X_2d, allow_pickle=False)
 
 labels = list(range(0, 10))
 target_ids = range(10)
@@ -69,19 +94,38 @@ colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'aquamarine', 'orange', 'purple'
 #plt.scatter(X_2d[:limit, 0], X_2d[:limit, 1], s=5, c='r', label='normal')
 #plt.scatter(X_2d[limit:, 0], X_2d[limit:, 1], s=2, c='b', label='attacked')
 
-unatt = X_2d[:limit]
-att = X_2d[limit:]
+#unatt = X_2d[:limit]
+#att = X_2d[limit:]
 #unatt = X_2d
 
-
-
+#label for class
 for i, c, label in zip(target_ids, colors, labels):
-    ax.scatter(unatt[(labelling == i), 0],
-               unatt[(labelling == i), 1],
-               c=norms[(labelling == i)],
+    ax1.scatter(X_2d[(trainlabels[...] == i), 0],
+               X_2d[(trainlabels[...] == i), 1],
+               c=c,
+               label=label,
+               s=3,
+               picker=True)
+
+ax1.set_title("Test Data TSNE Embedding")
+
+#label for norms
+"""
+for i, c, label in zip(target_ids, colors, labels):
+    ax2.scatter(X_2d[(trainlabels == i), 0],
+               X_2d[(trainlabels == i), 1],
+               c=norms[(trainlabels == i)],
                s=3,
                picker=True,
                cmap='viridis')
+"""
+
+#for i, c, label in zip(target_ids, colors, labels):
+ax2.scatter(X_2d[..., 0],
+           X_2d[..., 1],
+           c=norms[...],
+           s=3,
+           cmap='viridis')
 
 #ax.scatter(att[..., 0],
 #           att[..., 1],
@@ -91,25 +135,29 @@ for i, c, label in zip(target_ids, colors, labels):
 #           picker=True)
 
 
-ax.scatter(unatt[idxs,0],unatt[idxs,1],
+cb = ax2.scatter(X_2d[idxs,0],X_2d[idxs,1],
            c='black',
            label="nearest",
            s=10,
            picker=True)
 
+title = "Model Prediction: %d, Actual Label: %d" % (prediction,truelabel)
+ax2.set_title(title)
+'''
 
-ax.scatter(att[idx,0],att[idx,1],
+ax2.scatter(adv[idx,0],att[idx,1],
            c='red',
            label="attacked",
            s=10,
            picker=True)
+'''
 
-p=ax.get_children()[2]
-plt.colorbar(p,ax=ax)
+#p=ax2.get_children()[2]
+plt.colorbar(cb,label="norm")
 #plt.clim(np.argmin(norms),np.argmax(norms))
 
 #ax.scatter(output_2d[:, 0], output_2d[:, 1], c=target)
-plt.legend()
+ax1.legend()
 plt.show()
 exit(0)
 
