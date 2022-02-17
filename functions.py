d544ed06f2804654f45c3a38d31b8f23b9ed7715
vogtalex <vogtalex@oregonstate.edu>
2022-02-17 13:20:46 -0800

    Added TSNE integration for mnist

---
+++ functions.py
@@ -0,0 +1,219 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import torch.nn.functional as F
+from os.path import exists
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def SoftCrossEntropyLoss(input, target):
+
+  logprobs = torch.nn.functional.log_softmax(input, dim = 1)
+  print("1")
+  print(logprobs)
+
+  val = -(target * logprobs).sum()
+  print("2")
+  print(val)
+  return val / input.shape[0]
+
+
+
+def plot_images(X,y,yp,M,N,name):
+    print(X.shape())
+    print(y.shape())
+    print(yp.shape())
+    print('hihihih')
+    exit()
+    plt.clf()
+    f,ax = plt.subplots(M,N, sharex=True, sharey=True, figsize=(N,M*1.3))
+    for i in range(M):
+        for j in range(N):
+            ax[i][j].imshow(1-X[i*N+j][0].cpu().numpy(), cmap="gray")
+            title = ax[i][j].set_title("Pred: {}".format(yp[i*N+j].max(dim=0)[1]))
+            plt.setp(title, color=('g' if yp[i*N+j].max(dim=0)[1] == y[i*N+j] else 'r'))
+            ax[i][j].set_axis_off()
+    plt.tight_layout()
+    #plt.savefig(name)
+    plt.show()
+
+
+def fgsm(model, X, y, epsilon):
+    """ Construct FGSM adversarial examples on the examples X"""
+    delta = torch.zeros_like(X, requires_grad=True)
+    loss = torch.nn.functional.nll_loss(model(X+delta), y)
+    #loss = nn.CrossEntropyLoss()(model(X + delta), y)
+    loss.backward()
+    """
+    print(delta.grad.detach().sign().cpu()[0,0,...])
+    plt.imshow(delta.grad.detach().sign().cpu()[0,0,...])
+    plt.savefig("test")
+    exit(0)
+   """
+
+    """plt.figure(figsize=(5,5))
+    plt.plot(epsilons, b, label='baseline')
+    plt.plot(epsilons, s, label='student')
+    plt.yticks(np.arange(0, 1.1, step=0.1))
+    plt.xticks(np.arange(lower, upper+step, step=step))
+    plt.title(title)
+    plt.xlabel("Epsilon")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()"""
+    return epsilon * delta.grad.detach().sign()
+
+
+# In[121]:
+
+
+def pgd(model, X, y, epsilon, alpha, num_iter):
+    """ Construct FGSM adversarial examples on the examples X"""
+    delta = torch.zeros_like(X, requires_grad=True)
+    for t in range(num_iter):
+        loss = torch.nn.functional.nll_loss(model(X+delta), y)
+        #loss = nn.CrossEntropyLoss()(model(X + delta), y)
+        #print(loss)
+        loss.backward()
+        delta.data = (delta + X.shape[0]*alpha*delta.grad.data).clamp(-epsilon,epsilon)
+        delta.grad.zero_()
+    return delta.detach()
+
+
+# In[14]:
+
+
+def pgd_linf(model, X, y, epsilon, alpha, num_iter):
+    """ Construct FGSM adversarial examples on the examples X"""
+    delta = torch.zeros_like(X, requires_grad=True)
+    for t in range(num_iter):
+        loss = torch.nn.functional.nll_loss(model(X+delta), y)
+        #loss = nn.CrossEntropyLoss()(model(X + delta), y)
+        #print("loss")
+        #print(loss)
+        loss.backward()
+        #print(delta.grad.detach())
+        delta.data = (delta + alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
+        delta.grad.zero_()
+    return delta.detach()
+
+
+# In[15]:
+
+
+def pgd_linf_targ(model, X, y, epsilon, alpha, num_iter, y_targ):
+    """ Construct targeted adversarial examples on the examples X"""
+    delta = torch.zeros_like(X, requires_grad=True)
+    for t in range(num_iter):
+        yp = model(X + delta)
+        #print(yp)
+        loss = (yp[:,y_targ] - yp.gather(1,y[:,None])[:,0]).sum()
+        #print(yp[:,y_targ])
+        #print(yp.gather(1,y[:,None])[:,0])
+        #print(loss)
+        loss.backward()
+        delta.data = (delta + alpha*delta.grad.detach().sign()).clamp(-epsilon,epsilon)
+        #print(delta.data)
+        delta.grad.zero_()
+    return delta.detach()
+
+
+# In[82]:
+
+
+def epoch_adversarial(model, loader, attack, *args):
+    total_loss, total_err = 0.,0.
+    for X,y in loader:
+        X,y = X.to(device), y.to(device)
+        delta = attack(model, X, y, *args)
+        yp = model(X+delta)
+        loss = nn.CrossEntropyLoss()(yp,y)
+
+        total_err += (yp.max(dim=1)[1] != y).sum().item()
+        total_loss += loss.item() * X.shape[0]
+    return total_err / len(loader.dataset), total_loss / len(loader.dataset)
+
+def accuracyfinder(model, loader):
+    total_loss, total_err = 0.,0.
+    for X,y in loader:
+        X,y = X.to(device), y.to(device)
+        yp = model(X)
+        loss = nn.CrossEntropyLoss()(yp,y)
+
+        total_err += (yp.max(dim=1)[1] != y).sum().item()
+        total_loss += loss.item() * X.shape[0]
+    return total_err / len(loader.dataset), total_loss / len(loader.dataset)
+
+
+def norms(Z):
+    """Compute norms over all but the first dimension"""
+    return Z.view(Z.shape[0], -1).norm(dim=1)[:,None,None,None]
+
+
+def pgd_l2(model, X, y, epsilon, alpha, num_iter):
+    delta = torch.zeros_like(X, requires_grad=True)
+
+    for t in range(num_iter):
+        #_x_adv = x_adv.clone().detach().requires_grad_(True)
+        #loss = nn.CrossEntropyLoss()(model(X+delta), y)
+        loss = torch.nn.functional.nll_loss(model(X+delta), y)
+
+        #print(loss)
+        #print(model(X+delta).shape)
+        #print("loss")
+        #print(loss)
+        loss.backward()
+        #print(delta.data)
+        #print("delta data1")
+        #print("Info")
+        #print(alpha*delta.grad.detach())
+        #print(norms(delta.grad.detach()))
+        #exit(0)
+        delta.data += alpha*delta.grad.detach() / norms(delta.grad.detach())
+        #print(delta.data)
+        #print("delta data2")
+        delta.data = torch.min(torch.max(delta.detach(), -X), 1-X) # clip X+delta to [0,1]
+        #print(delta.data)
+        #print("delta data3")
+        delta.data *= epsilon / norms(delta.detach()).clamp(min=epsilon)
+        #print(delta.data)
+        #print("delta data4")
+        delta.grad.zero_()
+        #print(delta.data)
+
+    return delta.detach()
+    #return x_adv.detach()
+
+
+
+def create_graphs(epsilons, student, baseline, test_loader, title, pdf, attack, *args):
+    print(f'running {title}')
+    upper = max(epsilons)
+    lower = min(epsilons)
+    step = (upper + lower)/(len(epsilons) - 1)
+
+    students = []
+    baselines = []
+    for e in epsilons:
+        students.append(epoch_adversarial(student, test_loader, attack, e, *args)[0])
+        baselines.append(epoch_adversarial(baseline, test_loader, attack, e, *args)[0])
+
+    s = [(1 - a) for a in students]
+    b = [(1 - a) for a in baselines]
+
+    plt.figure(figsize=(5,5))
+    plt.plot(epsilons, b, label='baseline')
+    plt.plot(epsilons, s, label='student')
+    plt.yticks(np.arange(0, 1.1, step=0.1))
+    plt.xticks(np.arange(lower, upper+step, step=step))
+    plt.title(title)
+    plt.xlabel("Epsilon")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()
+
+    print(f'saving {pdf}')
+    plt.savefig(pdf)
---
+++ tsne_run.py
@@ -0,0 +1,259 @@
+# Load Python Libraries
+import numpy as np
+import gzip
+import pickle
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.manifold import TSNE
+from functions import *
+import torch
+limit = 60000
+idx = 7
+
+trainlabels = np.load('./npys/trainlabels.npy').astype(np.float64)[:limit]
+testlabels = np.load('./npys/testlabels.npy').astype(np.float64)[:limit]
+trainoutput = np.load('./npys/trainoutput.npy').astype(np.float64)[:limit]
+traindata = np.load('./npys/traindata.npy').astype(np.float64)[:limit]
+#traintarget = np.load('./npys/traintarget.npy')[:limit]
+
+advoutput = np.load('./npys/advoutput.npy').astype(np.float64)[:limit]
+advdata = np.load('./npys/advdata.npy').astype(np.float64)[:limit]
+
+
+def findNearest(data, advdata, idx, k=10):
+    print("Index: ", idx)
+    example = advdata[idx]
+    label = np.argmax(advoutput[idx])
+    print("Model prediction: ", label)
+
+    l = traindata - example
+
+    norms = np.linalg.norm(l, axis=1)
+
+    top = np.argpartition(norms, k-1)
+
+    ########
+    print("True label: ", int(testlabels[idx]))
+    print("Nearest 10 labels: ")
+    print(top[:k])
+    print([(int(trainlabels[i])) for i in top[:k]])
+    #print("Distance to nearest 10 points: ")
+    print([(norms[idx]) for idx in top[1:k]])
+    return norms, top[1:k], label, int(testlabels[idx])
+
+
+def findNearestTrue(data, advdata, idx, k=10):
+    print("Index: ", idx)
+    example = traindata[idx]
+    label = np.argmax(trainoutput[idx])
+    print("Model prediction: ", label)
+
+    l = traindata - example
+
+    norms = np.linalg.norm(l, axis=1)
+
+    top = np.argpartition(norms, k-1)
+
+    ########
+    print("True label: ", int(trainlabels[idx]))
+    print("Nearest 10 labels: ")
+    print([(int(trainlabels[idx])) for idx in top[:k]])
+    #print("Distance to nearest 10 points: ")
+    print([(norms[idx]) for idx in top[1:k]])
+    return norms, top[1:k], label, int(trainlabels[idx])
+
+
+norms, idxs, prediction, truelabel = findNearestTrue(traindata, advdata, idx)
+#norms,idxs,prediction,truelabel = findNearest(traindata,advdata,idx)
+
+print('data shape: ', traindata.shape)
+print('labels shape: ', trainlabels.shape)
+print('output shape: ', trainoutput.shape)
+# print(data[0])
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+print(traindata.shape)
+print(advdata.shape)
+# for combining data/advdata
+#data = np.append(data, advdata, axis=0)
+
+print(traindata.shape)
+
+X_2d = []
+if exists("./npys/embedding.npy"):
+    X_2d = np.load('./npys/embedding.npy').astype(np.float64)
+else:
+    tsne = TSNE(n_components=2, random_state=3, perplexity=100)
+    X_2d = tsne.fit_transform(traindata)
+    np.save('./npys/embedding.npy', X_2d, allow_pickle=False)
+
+labels = list(range(0, 10))
+target_ids = range(10)
+colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'aquamarine', 'orange', 'purple'
+
+#plt.scatter(X_2d[:limit, 0], X_2d[:limit, 1], s=5, c='r', label='normal')
+#plt.scatter(X_2d[limit:, 0], X_2d[limit:, 1], s=2, c='b', label='attacked')
+
+#unatt = X_2d[:limit]
+#att = X_2d[limit:]
+#unatt = X_2d
+
+# label for class
+for i, c, label in zip(target_ids, colors, labels):
+    ax1.scatter(X_2d[(trainlabels[...] == i), 0],
+                X_2d[(trainlabels[...] == i), 1],
+                c=c,
+                label=label,
+                s=3,
+                picker=True)
+
+ax1.set_title("Test Data TSNE Embedding")
+
+# label for norms
+"""
+for i, c, label in zip(target_ids, colors, labels):
+    ax2.scatter(X_2d[(trainlabels == i), 0],
+               X_2d[(trainlabels == i), 1],
+               c=norms[(trainlabels == i)],
+               s=3,
+               picker=True,
+               cmap='viridis')
+"""
+
+# for i, c, label in zip(target_ids, colors, labels):
+ax2.scatter(X_2d[..., 0],
+            X_2d[..., 1],
+            c=norms[...],
+            s=3,
+            cmap='viridis')
+
+# ax.scatter(att[..., 0],
+#           att[..., 1],
+#           c='fuchsia',
+#           label="attacked",
+#           s=3,
+#           picker=True)
+
+
+cb = ax2.scatter(X_2d[idxs, 0], X_2d[idxs, 1],
+                 c='black',
+                 label="nearest",
+                 s=10,
+                 picker=True)
+
+title = "Model Prediction: %d, Actual Label: %d" % (prediction, truelabel)
+ax2.set_title(title)
+'''
+
+ax2.scatter(adv[idx,0],att[idx,1],
+           c='red',
+           label="attacked",
+           s=10,
+           picker=True)
+'''
+
+# p=ax2.get_children()[2]
+plt.colorbar(cb, label="norm")
+# plt.clim(np.argmin(norms),np.argmax(norms))
+
+#ax.scatter(output_2d[:, 0], output_2d[:, 1], c=target)
+ax1.legend()
+plt.savefig('./tsne_output.png', bbox_inches='tight')
+plt.show()
+exit(0)
+
+"""
+def onclick(event):
+    print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+          ('double' if event.dblclick else 'single', event.button,
+           event.x, event.y, event.xdata, event.ydata))
+
+
+def onpick(event):
+    mouseevent = event.mouseevent
+    thispoint = event.artist
+    print("bloop")
+    of = thispoint.get_offsets()[0]
+    print(of)
+    #idx = list(X_2d).index(of.all())
+    idx = np.argwhere(X_2d == of)[0][0]
+    print(data.shape)
+    np.expand_dims(data,axis=(0,1))
+    print(data.shape)
+    exit()
+    plot_images(data[idx],target[idx][0],output[idx],3,6,"deleteme.png")
+
+
+labels = list(range(0, 10))
+target_ids = range(10)
+colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'aquamarine', 'orange', 'purple'
+unattacked = X_2d[:limit]
+attacked = X_2d[limit:]
+
+
+pairs = zip(xs,zs)
+
+for i, c, label in zip(target_ids, colors, labels):
+    ax.scatter(X_2d[(target[...,0] == i), 0],
+               X_2d[(target[...,0] == i), 1],
+               c=c,
+               label=label,
+               s=3,
+               picker=True)
+
+
+plt.legend()
+plt.savefig('./coloredavn.png', bbox_inches='tight')
+#fig.canvas.mpl_connect('button_press_event',onclick)
+fig.canvas.mpl_connect('pick_event',onpick)
+#fig.canvas.callbacks.connect('pick_event',on_pick)
+plt.show()
+exit()
+
+'''
+for i, c, label in zip(target_ids, colors, labels):
+    xs = X_2d[100:]
+    plt.scatter(xs[(advtarget[...,0] == i), 0], xs[(advtarget[...,0] == i), 1], s=3, c=c)
+
+'''
+#plt.scatter(X_2d[:10000, 0], X_2d[:10000, 1], s=5, c='r', label='normal')
+#plt.scatter(X_2d[10000:, 0], X_2d[10000:, 1], s=2, c='b', label='attacked')
+plt.legend()
+plt.savefig('./coloredavn.png', bbox_inches='tight')
+plt.show()
+exit()
+
+labels = list(range(0, 10))
+target_ids = range(10)
+colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'aquamarine', 'orange', 'purple'
+
+tsne = TSNE(n_components=2, random_state=1)
+X_2d = tsne.fit_transform(output)
+
+for i, c, label in zip(target_ids, colors, labels):
+    plt.scatter(X_2d[target[...,0] == i, 0], X_2d[target[...,0] == i, 1], s=10, c=c, label=label)
+plt.legend()
+plt.savefig('./x2d.png', bbox_inches='tight')
+plt.show()
+
+exit()
+
+plt.scatter(output_2d[:, 0], output_2d[:, 1], c=target)
+target_ids = range(len(data.target_names))
+
+
+exit()
+
+output_2d = bh_sne(output)
+
+np.save('./output_2d.npy', output_2d, allow_pickle=False)
+
+
+plt.rcParams['figure.figsize'] = 20, 20
+plt.scatter(output_2d[:, 0], output_2d[:, 1], c=target)
+plt.legend()
+plt.savefig('./output_2d.png', bbox_inches='tight')
+plt.show()
+
+"""
