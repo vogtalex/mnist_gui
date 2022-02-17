2c0eed500224c611b6ef3fde8f4268d5a50cdc2f
millsaps <millsaps@oregonstate.edu>
2022-02-17 10:25:40 -0800

    run_gui.py

--- run_gui.py
+++ run_gui.py
@@ -1,15 +1,15 @@
+
+import os
+os.environ['OPENBLAS_NUM_THREADS']='5'
+import torch
 from functions import *
 from models.distilled import *
-
-import torch
 import numpy as np
-import os
 #import matplotlib.pyplot as plt
 
 import time
 import tkinter as tk
 
-import torch
 import torch.onnx as onnx
 import torchvision.models as models
 import torch.nn as nn
@@ -21,17 +21,17 @@ import numpy as np
 import torchvision
 from torch.autograd import Variable
 
-import tkinter as tk
-from tkinter import *
-
-from PIL import Image, ImageTk
+#import tkinter as tk
+#from tkinter import *
+#
+#from PIL import Image, ImageTk
 
 from functools import partial
 
 #mtpltlib bug:
-matplotlib.use('TkAgg')
-from matplotlib import pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#matplotlib.use('TkAgg')
+#from matplotlib import pyplot as plt
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 test_loader = torch.utils.data.DataLoader(
@@ -42,8 +42,9 @@ test_loader = torch.utils.data.DataLoader(
 
 # Starter code if using cuda
 use_cuda = True
-device = torch.device("cuda:0" if (
-    use_cuda and torch.cuda.is_available()) else "cpu")
+#device = torch.device("cuda:0" if (
+    #use_cuda and torch.cuda.is_available()) else "cpu")
+devide = torch.device("cuda:0")
 
 # Use a pretrained model
 pretrained_model = "model_weights.pth"
@@ -72,7 +73,73 @@ def fgsm_attack(image, epsilon, data_grad):
     # Return the perturbed image
     return perturbed_image
 
-def gen_adv_features():
+def gen_adv_features_test():
+    model.eval()
+    cnt = 0
+
+    labels = []
+
+    out_data = []
+    out_adv_data= []
+
+    #out_target = []
+
+    #out_output = []
+    out_adv_output = []
+
+    for data, target in test_loader:
+        data,target = data.to(device), target.to(device)
+        cnt += 1
+        print("processing: %d/%d" % (cnt, len(test_loader.dataset)))
+        if cnt > 9000:
+            break;
+
+        delta = fgsm(model,data,target,0.2)
+        #delta = pgd_linf(model,data,target,0.05,1e-2,40)
+
+        #output = model(data)
+        adv_output = model(data + delta)
+
+        #output_np = output.data.cpu().numpy()
+        adv_output_np = adv_output.data.cpu().numpy()
+        #print(np.argmax(output_np),np.argmax(adv_output_np))
+
+
+        labels.append(target.cpu().numpy()[0])
+        #target_np = output.max(1, keepdim=True)[1][0].numpy()
+
+        adv_data = data+delta
+        ######################
+
+        #out_output.append(output_np)
+        out_adv_output.append(adv_output_np)
+
+        #out_target.append(target_np[:, np.newaxis])
+
+        #data = torch.flatten(data,2,3)
+        #data = torch.flatten(data,0)
+        #out_data.append(data.numpy())
+
+        adv_data = torch.flatten(adv_data,2,3)
+        adv_data = torch.flatten(adv_data,0)
+        out_adv_data.append(adv_data.cpu().numpy())
+
+
+    #output_array = np.concatenate(out_output, axis=0)
+    adv_output_array = np.concatenate(out_adv_output, axis=0)
+    #target_array = np.concatenate(out_target, axis=0)
+
+    #np.save('./npys/output.npy', output_array, allow_pickle=False)
+    np.save('./npys/e1/testlabels.npy', labels, allow_pickle=False)
+    np.save('./npys/e1/advoutput.npy', adv_output_array, allow_pickle=False)
+    #np.save('./npys/target.npy', target_array, allow_pickle=False)
+    #np.save('./npys/data.npy',out_data,allow_pickle=False)
+    np.save('./npys/e1/advdata.npy',out_adv_data,allow_pickle=False)
+    #torch.save(out_data,'./npys/data.npy')
+    #torch.save(out_adv_data,'./npys/advdata.npy')
+
+
+def gen_adv_features_train():
     model.eval()
     cnt = 0
 
@@ -85,29 +152,29 @@ def gen_adv_features():
 
     out_output = []
     out_adv_output = []
-    for data, target in test_loader:
+    for data, target in train_loader:
         cnt += 1
-        print("processing: %d/%d" % (cnt, len(test_loader.dataset)))
+        print("processing: %d/%d" % (cnt, len(train_loader.dataset)))
 
         #delta = fgsm(model,data,target,0.2)
-        delta = pgd_linf(model,data,target,0.1,1e-2,40)
+        #delta = pgd_linf(model,data,target,0.1,1e-2,40)
 
         output = model(data)
-        adv_output = model(data + delta)
+        #adv_output = model(data + delta)
 
-        output_np = output.data.cpu().numpy()
-        adv_output_np = adv_output.data.cpu().numpy()
+        output_np = output.data.numpy()
+        #adv_output_np = adv_output.data.cpu().numpy()
         #print(np.argmax(output_np),np.argmax(adv_output_np))
 
 
         labels.append(target.numpy()[0])
         target_np = output.max(1, keepdim=True)[1][0].numpy()
 
-        adv_data = data+delta
+        #adv_data = data+delta
         ######################
 
         out_output.append(output_np)
-        out_adv_output.append(adv_output_np)
+        #out_adv_output.append(adv_output_np)
 
         out_target.append(target_np[:, np.newaxis])
 
@@ -115,26 +182,26 @@ def gen_adv_features():
         data = torch.flatten(data,0)
         out_data.append(data.numpy())
 
-        adv_data = torch.flatten(adv_data,2,3)
-        adv_data = torch.flatten(adv_data,0)
-        out_adv_data.append(adv_data.numpy())
+        #adv_data = torch.flatten(adv_data,2,3)
+        #adv_data = torch.flatten(adv_data,0)
+        #out_adv_data.append(adv_data.numpy())
 
 
     output_array = np.concatenate(out_output, axis=0)
-    adv_output_array = np.concatenate(out_adv_output, axis=0)
+    #adv_output_array = np.concatenate(out_adv_output, axis=0)
     target_array = np.concatenate(out_target, axis=0)
 
-    np.save('./npys/output.npy', output_array, allow_pickle=False)
-    np.save('./npys/labels.npy', labels, allow_pickle=False)
-    np.save('./npys/advoutput.npy', adv_output_array, allow_pickle=False)
-    np.save('./npys/target.npy', target_array, allow_pickle=False)
-    np.save('./npys/data.npy',out_data,allow_pickle=False)
-    np.save('./npys/advdata.npy',out_adv_data,allow_pickle=False)
+    np.save('./npys/trainoutput.npy', output_array, allow_pickle=False)
+    np.save('./npys/trainlabels.npy', labels, allow_pickle=False)
+    #np.save('./npys/advoutput.npy', adv_output_array, allow_pickle=False)
+    np.save('./npys/traintarget.npy', target_array, allow_pickle=False)
+    np.save('./npys/traindata.npy',out_data,allow_pickle=False)
+    #np.save('./npys/advdata.npy',out_adv_data,allow_pickle=False)
     #torch.save(out_data,'./npys/data.npy')
     #torch.save(out_adv_data,'./npys/advdata.npy')
 
-
-gen_adv_features()
+gen_adv_features_test()
+#gen_adv_features_train()
 exit(0)
 
 
