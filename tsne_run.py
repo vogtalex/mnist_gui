338eabf2f69793e08a70d9db146d03dbfc171443
Simon Millsap <simonmillsap@gmail.com>
2022-02-22 13:02:34 -0800

    histograms now

--- tsne_run.py
+++ tsne_run.py
@@ -11,23 +11,27 @@ limit = 10000
 idx = 6
 
 npys = './npys'
-eps = 'e3'
+eps = 'e1'
 examples = 'examples'
 
-#train data
-trainlabels = np.load(os.path.join(npys,'trainlabels.npy')).astype(np.float64)[:limit]
-trainoutput = np.load(os.path.join(npys,'trainoutput.npy')).astype(np.float64)[:limit]
-traindata = np.load(os.path.join(npys,'traindata.npy')).astype(np.float64)[:limit]
+def get_data(npys,eps,examples):
+    #train data
+    trainlabels = np.load(os.path.join(npys,'trainlabels.npy')).astype(np.float64)[:limit]
+    trainoutput = np.load(os.path.join(npys,'trainoutput.npy')).astype(np.float64)[:limit]
+    traindata = np.load(os.path.join(npys,'traindata.npy')).astype(np.float64)[:limit]
+    
+    #adversarial data
+    testlabels = np.load(os.path.join(npys,eps,'testlabels.npy')).astype(np.float64)[:limit]
+    advoutput = np.load(os.path.join(npys,eps,'advoutput.npy')).astype(np.float64)[:limit]
+    advdata = np.load(os.path.join(npys,eps,'advdata.npy')).astype(np.float64)[:limit]
+    
+    #example data
+    exlabels = np.load(os.path.join(npys,examples,'testlabels.npy')).astype(np.float64)[:limit]
+    exoutput = np.load(os.path.join(npys,examples,'advoutput.npy')).astype(np.float64)[:limit]
+    exdata = np.load(os.path.join(npys,examples,'advdata.npy')).astype(np.float64)[:limit]
+    return trainlabels, trainoutput, traindata, testlabels, advoutput, advdata, exlabels, exoutput, exdata
 
-#adversarial data
-testlabels = np.load(os.path.join(npys,eps,'testlabels.npy')).astype(np.float64)[:limit]
-advoutput = np.load(os.path.join(npys,eps,'advoutput.npy')).astype(np.float64)[:limit]
-advdata = np.load(os.path.join(npys,eps,'advdata.npy')).astype(np.float64)[:limit]
-
-#example data
-exlabels = np.load(os.path.join(npys,examples,'testlabels.npy')).astype(np.float64)[:limit]
-exoutput = np.load(os.path.join(npys,examples,'advoutput.npy')).astype(np.float64)[:limit]
-exdata = np.load(os.path.join(npys,examples,'advdata.npy')).astype(np.float64)[:limit]
+trainlabels, trainoutput, traindata, testlabels, advoutput, advdata, exlabels, exoutput, exdata = get_data(npys,eps,examples)
 
 print("advdata ",advdata.shape)
 print("testlabels ",testlabels.shape)
@@ -47,7 +51,6 @@ def findNearest():
 
     top = np.argpartition(norms,k-1)
 
-    ########
     print("True label: ", int(exlabels[idx]))
     print("Nearest 10 labels: ")
     print(top[:k])
@@ -63,25 +66,122 @@ print('data shape: ', traindata.shape)
 print('labels shape: ', trainlabels.shape)
 print('output shape: ', trainoutput.shape)
 
-fig, (ax1,ax2) = plt.subplots(1,2)
 
 
 #for combining data/advdata
 #data = np.append(data, advdata, axis=0)
 
 X_2d = []
-#if exists("./npys/e1/embedding.npy"):
 if exists(os.path.join(npys,eps,'embedding.npy')):
-    #X_2d = np.load('./npys/e1/embedding.npy').astype(np.float64)
     X_2d = np.load(os.path.join(npys,eps,'embedding.npy')).astype(np.float64)
 else:
     tsne = TSNE(n_components=2, random_state=3,perplexity=100)
     X_2d = tsne.fit_transform(traindata)
     np.save('./embedding.npy', X_2d, allow_pickle=False)
 
+###HISTOGRAMS###################
+"""EPSILON 0.1"""
+
+trainlabels, trainoutput, traindata, testlabels, advoutput, advdata, exlabels, exoutput, exdata = get_data(npys,'e1',examples)
+norms,idxs,prediction,truelabel = findNearest()
+fig1, axs1 = plt.subplots(10)
+
+for i in range(10):
+    axs1[i].hist(norms[(testlabels[...] == i)], bins=40,range=(8,14),density=True)
+    axs1[i].text(13,.25,str(i),ha='center')
+
+title = "Epsilon: 0.1\nModel Prediction: %d\nActual Label: %d\nAverage Distance: %f" % (prediction,truelabel,float(sum(norms))/len(norms))
+plt.suptitle(title)
+
+##################
+"""EPSILON 0.2"""
+
+trainlabels, trainoutput, traindata, testlabels, advoutput, advdata, exlabels, exoutput, exdata = get_data(npys,'e2',examples)
+norms,idxs,prediction,truelabel = findNearest()
+fig2, axs2 = plt.subplots(10)
+
+for i in range(10):
+    axs2[i].hist(norms[(testlabels[...] == i)], bins=40,range=(8,14),density=True)
+    axs2[i].text(13,.25,str(i),ha='center')
+
+title = "Epsilon: 0.2\nModel Prediction: %d\nActual Label: %d\nAverage Distance: %f" % (prediction,truelabel,float(sum(norms))/len(norms))
+plt.suptitle(title)
+
+###########
+"""EPSILON 0.3"""
+
+trainlabels, trainoutput, traindata, testlabels, advoutput, advdata, exlabels, exoutput, exdata = get_data(npys,'e3',examples)
+norms,idxs,prediction,truelabel = findNearest()
+
+fig3, axs3 = plt.subplots(10)
+for i in range(10):
+    axs3[i].hist(norms[(testlabels[...] == i)], bins=40,range=(8,14),density=True)
+    axs3[i].text(13,.25,str(i),ha='center')
+
+title = "Epsilon: 0.3\nModel Prediction: %d\nActual Label: %d\nAverage Distance: %f" % (prediction,truelabel,float(sum(norms))/len(norms))
+plt.suptitle(title)
+
+##############
+
+plt.show()
+exit()
+
+
+def plot_embedding(labels,target_ids,colors,X_2d,testlabels,norms,idxs):
+    fig, (ax1,ax2) = plt.subplots(1,2)
+
+    #plot embedding for class coloration
+    for i, c, label in zip(target_ids, colors, labels):
+        ax1.scatter(X_2d[(testlabels[...] == i), 0],
+                   X_2d[(testlabels[...] == i), 1],
+                   c=c,
+                   label=label,
+                   s=3,
+                   picker=True)
+
+    ax1.set_title("Test Data")
+
+    #plot embedding for norm coloration
+    ax2.scatter(X_2d[..., 0],
+               X_2d[..., 1],
+               c=norms[...],
+               s=3,
+               cmap='viridis')
+
+    #plot 10 nearest points
+    cb = ax2.scatter(X_2d[idxs,0],X_2d[idxs,1],
+               c='black',
+               label="nearest",
+               s=10,
+               picker=True)
+
+    title = "Model Prediction: %d\nActual Label: %d\nAverage Distance: %f" % (prediction,truelabel,float(sum(norms))/len(norms))
+    ax2.set_title(title)
+
+    plt.colorbar(cb,label="norm")
+    cb.set_clim(5,15)
+    
+    ax1.legend()
+    plt.show()
+
+
 labels = list(range(0, 10))
 target_ids = range(10)
 colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'aquamarine', 'orange', 'purple'
+#plot_embedding(labels,target_ids,colors,X_2d,testlabels,norms,idxs)
+
+print('max distance', max(norms))
+print('min distance', min(norms))
+print('avg distance', sum(norms)/len(norms))
+
+
+
+
+
+
+
+#####OLD FUNCTIONS BELOW############
+
 
 #plt.scatter(X_2d[:limit, 0], X_2d[:limit, 1], s=5, c='r', label='normal')
 #plt.scatter(X_2d[limit:, 0], X_2d[limit:, 1], s=2, c='b', label='attacked')
@@ -97,15 +197,6 @@ for i, c, label in zip(target_ids, colors, labels):
                picker=True)
 '''
 
-for i, c, label in zip(target_ids, colors, labels):
-    ax1.scatter(X_2d[(testlabels[...] == i), 0],
-               X_2d[(testlabels[...] == i), 1],
-               c=c,
-               label=label,
-               s=3,
-               picker=True)
-
-ax1.set_title("Test Data")
 
 #label for norms
 """
@@ -119,11 +210,6 @@ for i, c, label in zip(target_ids, colors, labels):
 """
 
 #for i, c, label in zip(target_ids, colors, labels):
-ax2.scatter(X_2d[..., 0],
-           X_2d[..., 1],
-           c=norms[...],
-           s=3,
-           cmap='viridis')
 
 #ax.scatter(att[..., 0],
 #           att[..., 1],
@@ -133,18 +219,8 @@ ax2.scatter(X_2d[..., 0],
 #           picker=True)
 
 
-cb = ax2.scatter(X_2d[idxs,0],X_2d[idxs,1],
-           c='black',
-           label="nearest",
-           s=10,
-           picker=True)
 
-print('max distance', max(norms))
-print('min distance', min(norms))
-print('avg distance', sum(norms)/len(norms))
 
-title = "Model Prediction: %d\nActual Label: %d\nAverage Distance: %f" % (prediction,truelabel,float(sum(norms))/len(norms))
-ax2.set_title(title)
 
 '''
 ax2.scatter(adv[idx,0],att[idx,1],
@@ -155,12 +231,6 @@ ax2.scatter(adv[idx,0],att[idx,1],
 '''
 
 #p=ax2.get_children()[2]
-plt.colorbar(cb,label="norm")
-cb.set_clim(5,15)
-
-#ax.scatter(output_2d[:, 0], output_2d[:, 1], c=target)
-ax1.legend()
-plt.show()
 
 
 def onpick(event):
